ce93b8794fe30d28a9a68bce87f81a490cc63563
bobthechemist <bobthechemist@gmail.com>
2021-06-25 08:28:43 -0400

    docs
    sticking with default theme (alabaster)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+extensions = ['sphinx.ext.autodoc', 'sphinx_book_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +47,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+#html_theme = 'sphinx_book_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
