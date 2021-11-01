13f46336356e780dde96fd99f6e0e0af0b41d51a
bobthechemist <bobthechemist@gmail.com>
2021-11-01 11:30:53 -0400

    bumped doc version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, BoB LeSuer (BoBthechemist)'
 author = 'BoB LeSuer (BoBthechemist)'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.2.0'
 
 
 
